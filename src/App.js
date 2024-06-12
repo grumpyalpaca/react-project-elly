44a8f25e3a83833e7c4993b2f934deda8cb05ad4
grumpyalpaca <119808526+grumpyalpaca@users.noreply.github.com>
2024-06-13 00:29:20 +0200

    Added box + corresponding CSS

--- src/App.js
+++ src/App.js
@@ -7,37 +7,39 @@ function App() {
   return (
     <div className="App">
       <div className="weather-app-wrapper">
-        <Search />
-        <Results />
-      </div>
+        <div class="weather-app">
+          <Search />
+          <Results />
+        </div>
 
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.shecodes.io/graduates/33063-elizabeth-ballesteros-bosworth"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Elizabeth Ballesteros Bosworth
-        </a>
-        , is open-sourced on{" "}
-        <a
-          href="https://github.com/grumpyalpaca/react-project-elly"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>{" "}
-        and website hosted on{" "}
-        <a
-          href="https://react-elly-project.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-        .
-      </footer>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/33063-elizabeth-ballesteros-bosworth"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Elizabeth Ballesteros Bosworth
+          </a>
+          , is open-sourced on{" "}
+          <a
+            href="https://github.com/grumpyalpaca/react-project-elly"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>{" "}
+          and website hosted on{" "}
+          <a
+            href="https://react-elly-project.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
