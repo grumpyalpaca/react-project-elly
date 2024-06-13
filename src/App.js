a5c0c73326b9fdd79294683b75a41cd222025815
grumpyalpaca <119808526+grumpyalpaca@users.noreply.github.com>
2024-06-13 21:12:46 +0200

    Added bootstrap correctly and amended css a bit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
 import React from "react";
 import Weather from "./Weather";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Barcelona" />
+        <div className="weather-app-wrapper">
+          <Weather defaultCity="Barcelona" />
 
-        <footer>
-          This project was coded by{" "}
-          <a
-            href="https://www.shecodes.io/graduates/33063-elizabeth-ballesteros-bosworth"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Elizabeth Ballesteros Bosworth
-          </a>{" "}
-          and is open-sourced on{" "}
-          <a
-            href="https://github.com/grumpyalpaca/react-project-elly"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>{" "}
-          and hosted on{" "}
-          <a
-            href="https://react-elly-project.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Netlify
-          </a>
-        </footer>
+          <footer>
+            This project was coded by{" "}
+            <a
+              href="https://www.shecodes.io/graduates/33063-elizabeth-ballesteros-bosworth"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Elizabeth Ballesteros Bosworth
+            </a>{" "}
+            and is open-sourced on{" "}
+            <a
+              href="https://github.com/grumpyalpaca/react-project-elly"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://react-elly-project.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Netlify
+            </a>
+          </footer>
+        </div>
       </div>
     </div>
   );
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -13,11 +13,11 @@ export default function WeatherInfo(props) {
         </li>
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
-      <div className="row mt-3">
+      <div className="row">
         <div className="col-6">
           <div className="d-flex">
             <div>
-              <WeatherIcon code={props.data.icon} size={52} />
+              <WeatherIcon code={props.data.icon} size={40} />
             </div>
 
             <div>
